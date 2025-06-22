bbdce1e1ba2e34c6871bef7a5a36e0bcbae55c2c
Nitin Dixit <nitinsdixit03@gmail.com>
2025-06-22 21:58:00 +0530

    Update to use MONGO_URI from environment

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGOURL);
         console.log("✅ MongoDB is successfully connected!!");
     } catch (error) {
         console.error("❌ MongoDB connection failed:", error.message);
